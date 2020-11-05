2d5847846fbbe68061edf9dd53279af80e9c40b0
Michelle-Lefebvre <mmldesigns007@gmail.com>
2020-11-05 10:15:52 -0500

    created firestore database connection

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,31 +1,67 @@
 /* eslint-disable no-unused-vars */
-// eslint - disable react / jsx - no - undef;
+/* eslint-disable no-undef */
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './Post.js';
 import { db } from './firebase.js';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Modal from '@material-ui/core/Modal';
 
+// function getModalStyle() {
+//   const top = 50 + rand();
+//   const left = 50 + rand();
+
+//   return {
+//     top: `${top}%`,
+//     left: `${left}%`,
+//     transform: `translate(-${top}%, -${left}%)`,
+//   };
+// }
+
+// const useStyles = makeStyles((theme) => ({
+//   paper: {
+//     position: 'absolute',
+//     width: 400,
+//     backgroundColor: theme.palette.background.paper,
+//     border: '2px solid #000',
+//     boxShadow: theme.shadows[5],
+//     padding: theme.spacing(2, 4, 3),
+//   },
+// }));
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     // run once when post refreshes
     db.collection('posts').onSnapshot(snapshot => {
-      setPosts(snapshot.docs.map(doc => doc.data()));
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id,
+        post: doc.data()
+      })));
     })
 
   }, []); //run everytime the variable posts changes
 
   return (
     <div className="app">
+      {/* <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <h2>I am a modal</h2>
+        </div>
+      </Modal> */}
+
       <div className="app__header" >
         <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="instagram word" />
       </div>
 
       {
-        posts.map(post => (
-          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} altTxt={post.altTxt} />
+        posts.map(({ id, post }) => (
+          <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl} altTxt={post.altTxt} />
         ))
       }
 
--- instagram-clone/src/firebase.js
+++ instagram-clone/src/firebase.js
@@ -1,6 +1,6 @@
 import firebase from "firebase";
-const firebaseApp = firebase.initializeApp({
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
     apiKey: "AIzaSyATvKCWS7_KxrYZDdjP30-KOk_fs1V2IdY",
     authDomain: "instagramia.firebaseapp.com",
     databaseURL: "https://instagramia.firebaseio.com",
@@ -9,8 +9,9 @@ const firebaseApp = firebase.initializeApp({
     messagingSenderId: "367874954883",
     appId: "1:367874954883:web:2150e6b07721638ea9edb6",
     measurementId: "G-ZMZT31VB67"
-});
+};
 
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const storage = firebase.storage();
