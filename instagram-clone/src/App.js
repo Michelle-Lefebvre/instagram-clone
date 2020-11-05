2ee6f3c056fe4e2c0db3a5bf48b0f300321970bc
Michelle-Lefebvre <mmldesigns007@gmail.com>
2020-11-05 12:15:00 -0500

    added signup, auth

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -3,35 +3,45 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './Post.js';
-import { db } from './firebase.js';
-// import { makeStyles } from '@material-ui/core/styles';
-// import Modal from '@material-ui/core/Modal';
+import { db, auth } from './firebase.js';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import { Button, Input } from '@material-ui/core';
 
-// function getModalStyle() {
-//   const top = 50 + rand();
-//   const left = 50 + rand();
 
-//   return {
-//     top: `${top}%`,
-//     left: `${left}%`,
-//     transform: `translate(-${top}%, -${left}%)`,
-//   };
-// }
 
-// const useStyles = makeStyles((theme) => ({
-//   paper: {
-//     position: 'absolute',
-//     width: 400,
-//     backgroundColor: theme.palette.background.paper,
-//     border: '2px solid #000',
-//     boxShadow: theme.shadows[5],
-//     padding: theme.spacing(2, 4, 3),
-//   },
-// }));
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
+
   const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false);
+
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   useEffect(() => {
     // run once when post refreshes
@@ -44,20 +54,51 @@ function App() {
 
   }, []); //run everytime the variable posts changes
 
+  const signUp = (event) => {
+    event.preventDefault();
+    auth.createUserWithEmailAndPassword(email, password).catch((error) => alert(error.message));
+  }
+
   return (
     <div className="app">
-      {/* <Modal
+      <Modal
         open={open}
         onClose={() => setOpen(false)}
       >
         <div style={modalStyle} className={classes.paper}>
-          <h2>I am a modal</h2>
+          <form className="app__signup">
+            <center>
+              <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="instagram word" />
+            </center>
+            <input
+              placeholder="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" onClick={signUp}>Sign Up</Button>
+
+          </form>
         </div>
-      </Modal> */}
+      </Modal>
 
       <div className="app__header" >
         <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="instagram word" />
       </div>
+
+      <Button onClick={() => setOpen(true)}>Sign Up</Button>
 
       {
         posts.map(({ id, post }) => (
